1390a41e99846c711a637c3fe9aec84d2efe1be1
Peter Rice <p.rice@imperial.ac.uk>
2017-03-27 17:26:19 +0100

    use 16.1 plus etriks 16.1.30 plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,8 +25,9 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-        grailsCentral()
+        mavenRepo "file://${System.getProperty('user.home')}/m2local/etriksv3"
         mavenLocal()
+        grailsCentral()
         mavenCentral()
 
         mavenRepo "https://repo.transmartfoundation.org/content/repositories/public/"
@@ -49,8 +50,8 @@ grails.project.dependency.resolution = {
         }
         runtime ':resources:1.2.1'
         runtime ':hibernate:3.6.10.7'
-        compile ':rdc-rmodules:1.2.2-SNAPSHOT'
-        compile ':transmart-legacy-db:1.2.2-SNAPSHOT'
+        compile ':rdc-rmodules:16.1.30'
+        compile ':transmart-legacy-db:16.1'
         build ':tomcat:7.0.50'
     }
 
